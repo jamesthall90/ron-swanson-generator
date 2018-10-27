2dac06c4f2b25fd107c899c56d270d481cd34334
jamesthall90 <jamesthall90@gmail.com>
2018-10-27 15:00:37 -0400

    Commented failing code

--- Models/AutoMapperModelProfile.cs
+++ Models/AutoMapperModelProfile.cs
@@ -1,15 +1,15 @@
-﻿using AutoMapper;
-using Models.Domain;
-using Models.DTO;
-using Models.DTO.RonSwansonQuote;
-
-namespace Models
-{
-    public class AutoMapperModelProfile : Profile
-    {
-        public AutoMapperModelProfile()
-        {
-            CreateMap<RonSwansonQuote, RonSwansonQuoteDetailDto>();
-        }
-    }
-}
+﻿//using AutoMapper;
+//using Models.Domain;
+//using Models.DTO;
+//using Models.DTO.RonSwansonQuote;
+//
+//namespace Models
+//{
+//    public class AutoMapperModelProfile : Profile
+//    {
+//        public AutoMapperModelProfile()
+//        {
+//            CreateMap<RonSwansonQuote, RonSwansonQuoteDetailDto>();
+//        }
+//    }
+//}
